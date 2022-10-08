6accf86063cebe8dfb40459776a62bc74c6e916f
Miguel M <miguelmiguelezcazapal13@gmail.com>
2022-10-08 17:27:08 +0200

    08/10/2022_17:27

--- 01-multithreading/02-exercise/Ejercicio2/Ejercicio2/Program.cs
+++ 01-multithreading/02-exercise/Ejercicio2/Ejercicio2/Program.cs
@@ -6,48 +6,60 @@
         public static void MenuGenerator(string[] names, MyDelegate[] funciones)
         {
             bool flag = false;
-            int respuesta = 0;
+            int respuesta;
 
-            try
+            while (!flag)
             {
-                while (!flag)
+                try
                 {
+                    Console.WriteLine("Menu:");
                     for (int i = 0; i < names.Length; i++)
                     {
-                        Console.WriteLine($"{i}. {names[i]}");
+                        Console.WriteLine($"{i + 1,5}. {names[i]}");
                     }
-                    Console.WriteLine($"{names.Length}. Exit");
-                    flag = true;
+                    Console.WriteLine($"{names.Length + 1,5}. Exit");
+                    Console.Write("Enter option: ");
+                    respuesta = Convert.ToInt32(Console.ReadLine());
+
+                    if (respuesta != 4)
+                    {
+                        funciones[respuesta - 1]();
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        flag = true;
+                        Console.WriteLine("Thanks and come back soon!");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error, you don't use characters only numbers to the options\n");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Error, range excedeed\n");
                 }
             }
-            catch
-            {
-
-            }
         }
 
-        static void f1()
-        {
-            Console.WriteLine("A");
-        }
-        static void f2()
-        {
-            Console.WriteLine("B");
-        }
-        static void f3()
-        {
-            Console.WriteLine("C");
-        }
-
-
-
-
-
+        //static void f1()
+        //{
+        //    Console.WriteLine("A");
+        //}
+        //static void f2()
+        //{
+        //    Console.WriteLine("B");
+        //}
+        //static void f3()
+        //{
+        //    Console.WriteLine("C");
+        //}
 
         static void Main(string[] args)
         {
             MenuGenerator(new string[] { "Opción", "Opción", "Opción" },
-            new MyDelegate[] { f1, f2, f3 });
+            new MyDelegate[] {});
             Console.ReadKey();
         }
     }
