49ad129512502a2eb24dba699c50d7e419a59647
SrNuno <miguelmiguelezcazapal13@gmail.com>
2023-01-26 12:30:16 +0100

    26/01/2023_12:30

---
+++ 03-networking/01-exercise/Cliente/Client.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Cliente
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            const string IP_SERVER = "127.0.0.1";
+            string msg;
+            string userMsg;
+            IPEndPoint ie = new IPEndPoint(IPAddress.Parse(IP_SERVER), 5005);
+            Console.WriteLine("Starting client. Press a key to init connection");
+            Socket server = new Socket(AddressFamily.InterNetwork,
+            SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ie);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Error connection: {0}\nError code: {1}({2})",
+                e.Message, (SocketError)e.ErrorCode, e.ErrorCode);
+                Console.ReadKey();
+                return;
+            }
+
+            using (NetworkStream ns = new NetworkStream(server))
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+            {
+                msg = sr.ReadLine();
+                Console.WriteLine(msg);
+                userMsg = Console.ReadLine();
+                sw.WriteLine(userMsg);
+                sw.Flush();
+                msg = sr.ReadLine();
+                Console.WriteLine(msg);
+            }
+            Console.WriteLine("Ending connection");
+            server.Close();
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ 03-networking/01-exercise/Ejercicio1/Servidor.cs
@@ -0,0 +1,87 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+
+namespace Ejercicio1
+{
+    internal class Servidor
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ie = new IPEndPoint(IPAddress.Any, 5005);
+            using (Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+            ProtocolType.Tcp))
+            {
+                s.Bind(ie);
+                s.Listen(10);
+                Console.WriteLine($"Server listening at port:{ie.Port}");
+                Socket sClient = s.Accept();
+                IPEndPoint ieClient = (IPEndPoint)sClient.RemoteEndPoint;
+                Console.WriteLine("Client connected:{0} at port {1}", ieClient.Address,
+               ieClient.Port);
+                using (NetworkStream ns = new NetworkStream(sClient))
+                using (StreamReader sr = new StreamReader(ns))
+                using (StreamWriter sw = new StreamWriter(ns))
+                {
+                    string welcome = "Welcome to Nuno Server";
+                    sw.WriteLine(welcome);
+                    sw.Flush();
+                    string msg = sr.ReadLine();
+                    while (msg != null)
+                    {
+                        try
+                        {
+                            string[] text = msg.Split(" ");
+                            switch (text[0])
+                            {
+                                case "time":
+                                    sw.WriteLine(DateTime.Now.ToString("h:mm"));
+                                    break;
+
+                                case "date":
+                                    sw.WriteLine(DateTime.UtcNow.ToString("dd-MM-yyyy"));
+                                    break;
+
+                                case "all":
+                                    sw.WriteLine(DateTime.Now);
+                                    break;
+
+                                case "close":
+                                    StreamReader reader = null;
+                                    try
+                                    {
+                                        reader = new StreamReader(Environment.GetEnvironmentVariable("PROGRAMDATA") + "\\password.txt");
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine(e.Message);
+                                    }
+
+                                    if (text.Length > 1 && text[1] == reader.ReadLine())
+                                    {
+                                        sw.WriteLine("Close operation");
+                                    }
+                                    else
+                                    {
+                                        sw.WriteLine("Password invalid");
+                                    }
+                                    break;
+
+                                default:
+                                    sw.WriteLine("Command invalid");
+                                    break;
+                            }
+                            sw.Flush();
+                        }
+                        catch (IOException)
+                        {
+                            msg = null;
+                        }
+                    }
+                    Console.WriteLine("Client disconnected.\nConnection closed");
+                }
+                //sClient.Close();
+            }
+        }
+    }
+}
